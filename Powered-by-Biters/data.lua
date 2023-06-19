9362dc84376124071f7fd069d092afeed0203797
Aivan <AivanF@users.noreply.github.com>
2023-06-19 19:54:35 +0300

    PBB v1.0.1

--- Powered-by-Biters/data.lua
+++ Powered-by-Biters/data.lua
@@ -223,16 +223,21 @@ to_oil.icon = "__Powered-by-Biters__/graphics/meet-to-oil.png"
 to_oil.icon_size = 64
 to_oil.category = "chemistry"
 to_oil.order = to_oil.order
-to_oil.ingredients = {{"raw-meat", 15}}
-to_oil.energy_required = 3
+to_oil.ingredients = {{"raw-meat", 10}}
+to_oil.energy_required = 4
 to_oil.results = {{
 	type = "fluid",
 	name = "crude-oil",
-	amount = 20,
+	amount = 30,
 }}
 data.raw.recipe[to_oil.name] = to_oil
 
-table.insert(data.raw.technology["advanced-oil-processing"].effects, {
+local prereq = "advanced-oil-processing"
+if settings.startup["af-meet-to-oil-early"].value then
+	prereq = "oil-processing"
+end
+
+table.insert(data.raw.technology[prereq].effects, {
 	type = "unlock-recipe",
 	recipe = to_oil.name,
 })
---
+++ Powered-by-Biters/settings.lua
@@ -0,0 +1,9 @@
+data:extend{
+    {
+        type = "bool-setting",
+        name = "af-meet-to-oil-early",
+        setting_type = "startup",
+        default_value = true,
+        order = "a",
+    },
+}
